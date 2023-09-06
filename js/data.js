bdf70491e37fcea4da1219054d5d30a737a3734c
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-06 12:30:29 +0530

    All Files Uploaded

---
+++ js/data.js
@@ -0,0 +1,23 @@
+let movies = [
+    {
+        image: 'img/banner/iccdesktop.avif'
+    },
+    {
+        image: 'img/banner/motogpsepdesktop.avif'
+    },
+    {
+        image: 'img/banner/playcardwebbanner.avif'
+    },
+    {
+        image: 'img/banner/streamsavesepdesktop.avif'
+    },
+    {
+        image: 'img/banner/banner5.webp'
+    },
+    {
+        image: 'img/banner/banner6.webp'
+    },
+    {
+        image: 'img/banner/et00357595-wspgmtrnnr-portrait.jpg'
+    }
+]
